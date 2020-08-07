279adc220e0d36cdb25c30f9a504a1cefca727ed
Raisul Islam Anik <60985479+Anik055-hub@users.noreply.github.com>
2020-08-07 11:29:54 +0600

    Add files via upload

---
+++ Controllers/AdminHomeController.cs
@@ -0,0 +1,58 @@
+﻿using Online_Learning.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Online_Learning.Controllers
+{
+    public class AdminHomeController : Controller
+    {
+        OnlineLearningEntities1 userRepo  = new OnlineLearningEntities1();
+        [HttpGet]
+        public ActionResult Admin()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult ProfileInfo(int id)
+        {
+            var result = from item in userRepo.Users
+                         where item.UserID == id
+                         select item;
+          //    User profile = result.FirstOrDefault();
+          //  User p = userRepo.Users.Where(x => x.UserID == id).FirstOrDefault();
+          //  p.UserID = id;
+          //  return View(p);
+            return View(result.FirstOrDefault());
+        }
+        [HttpGet]
+        public ActionResult Teachers()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Students()
+        {
+            return View();
+        }
+        
+        [HttpGet]
+        public ActionResult Courses()
+        {
+            return View(userRepo.Courses.ToList());
+        }
+        [HttpGet]
+        public ActionResult Blogs()
+        {
+            return View(userRepo.CourseMaterials.ToList());
+        }
+        [HttpGet]
+        public ActionResult Messages()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Controllers/LoginController.cs
@@ -0,0 +1,87 @@
+﻿using Online_Learning.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Online_Learning.Controllers
+{
+    public class LoginController : Controller
+    {
+        OnlineLearningEntities1 userRepo = new OnlineLearningEntities1();
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(User user)
+        {
+            if (ModelState.IsValid)
+            {
+              using (OnlineLearningEntities1 data = new OnlineLearningEntities1())
+              {
+                var obj = data.Users.Where(a => a.Username.Equals(user.Username) && a.Password.Equals(user.Password)).FirstOrDefault();
+                if (obj != null)
+                {
+                    Session["UserID"] = obj.UserID.ToString();
+                    Session["Username"] = obj.Username.ToString();
+                    //Session["UserType"] = obj.UserType.ToString();
+                    if (obj.UserType.ToString() == "admin")
+                        
+                            return RedirectToAction("AdminDashboard");
+                          
+                    else if (obj.UserType.ToString() == "teacher")
+                        
+                            return RedirectToAction("TeacherDashboard");
+                        
+                    else if (obj.UserType.ToString() == "student")
+                        
+                            return RedirectToAction("StudentDashboard");
+                        
+                }
+              }
+            }
+          return RedirectToAction("Login");
+        }
+        [HttpGet]
+        public ActionResult AdminDashboard()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult TeacherDashboard()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult StudentDashboard()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Login");
+        }
+        [HttpGet]
+        public ActionResult SignUp()
+        {
+            User[] users = userRepo.Users.ToArray();
+            ViewData["users"] = users;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignUp(User u)
+        {
+            userRepo.Users.Add(u);
+            userRepo.SaveChanges();
+            return RedirectToAction("Login");
+        }
+    }
+}
